2a119d447719abf15aced11cc3cd4cb7d84a9791
Elanas <elanasinfo@gmail.com>
2018-01-30 14:34:16 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.print("Iveskite Savo Varda ");
+        Scanner scanner = new Scanner(System.in);
+
+        String vardas = scanner.nextLine();
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
     }
 }
